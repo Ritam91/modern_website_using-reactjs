345a9e4346570bf6e3c85870029e48c5777fa8b4
Ritam91 <ritamkumarsinha999@gmail.com>
2023-12-25 23:13:34 +0530

    updated feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './feature.css';
-const Feature = () => {
+const Feature = ({title , text}) => {
   return (
-    <div>Feature</div>
+    <div className="features__container-feature">
+      <div className="features__container-feature-title">
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className="features__container-feature-text">
+        {text}
+      </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT/WhatGPT.jsx
+++ src/containers/whatGPT/WhatGPT.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import './whatGPT.css';
-import { Feature } from '../../components';
+import React from "react";
+import "./whatGPT.css";
+import { Feature } from "../../components";
 const Whatgpt = () => {
   return (
     <div className="whatgpt section__margin" id="wgpt">
       <div className="whatgpt__feature">
-        <Feature/>
+        <Feature
+          title="What is GPT?"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing Lorem ipsum dolor sit amet consectetur adipisi Lorem ipsum dolor sit amet consectetur adipisi elit. Dolor recusandae cum laboriosam numquam."
+        />
       </div>
 
       <div className="whatgpt__heading">
-        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
         <p>Explore the Library</p>
       </div>
 
       <div className="whatgpt__container">
-        <Feature/>
-        <Feature/>
-        <Feature/>
+        <Feature
+          title="Chatbots"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor recusandae cum laboriosam numquam."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor recusandae cum laboriosam numquam."
+        />
+        <Feature
+          title="Education"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor recusandae cum laboriosam numquam."
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Whatgpt
+export default Whatgpt;
