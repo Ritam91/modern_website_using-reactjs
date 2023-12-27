79c6df06802d6409f490517a38e5b022f53d7aac
Ritam91 <ritamkumarsinha999@gmail.com>
2023-12-28 00:10:17 +0530

    Files Updated

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './cta.css';
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className="cta">
+      <div className="cta__content">
+        <p>Request Early Access</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+
+      <div className="cta__btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
